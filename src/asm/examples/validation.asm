7b9514bcc27fde55567999c807bce47c55f9e57e
Dean Kilfoyle <dkilfoyle@xtra.co.nz>
2025-10-11 15:25:03 +1300

    validate immediate argument is in range

---
+++ src/asm/examples/validation.asm
@@ -0,0 +1,15 @@
+; correct
+ld a, 10
+
+; missing argument
+ld a
+
+; too many arguments
+ld a,a,a
+
+; wrong argument
+ld 10, a
+
+; out of range argument
+ld a, 299
+
--- src/asm/language/asm-validator.ts
+++ src/asm/language/asm-validator.ts
@@ -1,5 +1,5 @@
 import type { ValidationAcceptor, ValidationChecks } from "langium";
-import { type AsmAstType, Expression, Instruction, Label } from "./generated/ast.js";
+import { type AsmAstType, Expression, Instruction, isBinaryExpression, Label } from "./generated/ast.js";
 import type { AsmServices } from "./asm-module.js";
 import { userPreferences } from "./asm-userpreferences.js";
 import { IOpcodesNode, opcodesLookup } from "../opcodes-z80.js";
@@ -30,10 +30,15 @@ export class AsmValidator {
   checkInstructionArgs(instruction: Instruction, accept: ValidationAcceptor) {
     const instrName = instruction.opcode.toUpperCase();
     const args = (instruction.expressionList?.expressions || []).map((expr) => {
-      const toArgTemplate = (expr: Expression) => {
+      const toArgTemplate = (expr: Expression): string => {
+        if (isBinaryExpression(expr)) {
+          const left = expr.left.$cstNode!.text;
+          if (left == "IX" || left == "IY") return left + "+DD";
+          return "$NN";
+        }
         if (expr.immediate) return "$IMM";
         if (expr.label) return instrName == "JR" ? "$N" : "$NN";
-        if (expr.paren) return toArgTemplate(expr.paren);
+        if (expr.paren) return "(" + toArgTemplate(expr.paren) + ")";
         return expr.$cstNode!.text.toUpperCase();
       };
       return toArgTemplate(expr);
@@ -45,52 +50,40 @@ export class AsmValidator {
     for (let i = 0; i <= args.length; i++) {
       if (i == args.length) {
         if (curNode.codes.length == 0)
+          // check for not enough arms
           accept("error", "Missing argument " + Array.from(curNode.args.keys()).join(","), { node: instruction, property: "opcode" });
       } else {
+        const expr = instruction.expressionList!.expressions[i];
         const nextNode: IOpcodesNode | undefined =
-          args[i] == "$IMM" ? curNode.args.get("$N") || curNode.args.get("$NN") || curNode.args.get("$DD") : curNode.args.get(args[i]);
+          args[i] == "$IMM" ? curNode.args.get("$N") || curNode.args.get("$NN") : curNode.args.get(args[i]);
         if (!nextNode) {
+          // either too many args
           if (curNode.args.size == 0)
             accept("error", "Unexpected argument", {
-              node: instruction.expressionList!.expressions[i],
+              node: expr,
             });
           else
+            // or invalid argument type (ast != template)
             accept("error", "Invalid argument, expecting " + Array.from(curNode.args.keys()).join(","), {
-              node: instruction.expressionList!.expressions[i],
+              node: expr,
             });
           return;
+        } else {
+          // check for out of range
+          if (args[i] == "$IMM") {
+            if (curNode.args.get("$N"))
+              if (expr.immediate! < 0 || expr.immediate! > 255) accept("error", "Immediate value out of range 0-255", { node: expr });
+            if (curNode.args.get("$NN"))
+              if (expr.immediate! < 0 || expr.immediate! > 65535) accept("error", "Immediate value out of range 0-65535", { node: expr });
+          }
+          if (args[i] == "(IX+DD" || args[i] == "(IY+DD)")
+            if (isBinaryExpression(expr.paren) && expr.paren.right.immediate != undefined) {
+              if (expr.paren.right.immediate < -128 || expr.immediate! > 128)
+                accept("error", "Immediate value out of range -128-127", { node: expr });
+            }
         }
         curNode = nextNode!;
       }
     }
-    // const options = opcodesLookup.get(instruction.opcode.toUpperCase());
-    // if (!options) throw Error();
-    // const numArgs = instruction.expressionList ? instruction.expressionList.expressions.length : 0;
-    // const [minArgs, maxArgs] = options!.reduce(
-    //   (accum, cur) => {
-    //     if (cur.args.length < accum[0]) accum[0] = cur.args.length;
-    //     if (cur.args.length > accum[1]) accum[1] = cur.args.length;
-    //     return accum;
-    //   },
-    //   [5, 0]
-    // );
-    // if (numArgs < minArgs || numArgs > maxArgs) {
-    //   if (minArgs != maxArgs) accept("error", `Expected ${minArgs}-${maxArgs} arguments`, { node: instruction, property: "opcode" });
-    //   else accept("error", `Expected ${minArgs} arguments`, { node: instruction, property: "opcode" });
-    // }
-    // // match arg0
-    // const arg0 = instruction.expressionList?.expressions[0];
-    // if (arg0) {
-    //   const validArg0s = options.map((opt) => opt.args[0]).filter((x) => x).;
-    //   // find arg options starting with arg0
-    //   const arg0Matches = options.filter((opt) => {
-    //     if (arg0.register && opt.args[0] == arg0.register) return true;
-    //     if (arg0.immediate && opt.args[0].startsWith("$N")) return true;
-    //     return false;
-    //   });
-    //   console.log("arg0 matches", arg0Matches);
-    //   if (!arg0Matches.length)
-    //     accept("error", "Invalid argument, expecting 1 of " + validArg0s.join(","), { node: instruction.expressionList!.expressions[0] });
-    // }
   }
 }
